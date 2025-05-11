0ac3d52c30b868589656fe9259aa1cb14921f024
agustinasc <agustina.santillancastro@gmail.com>
2025-05-10 22:54:49 -0300

    Intento de corregir conexion de netlify con ProductsList

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,7 +7,8 @@ export const CartContext = createContext();
 
 export const useProducts = () => useContext(CartContext)
 
-const API = 'http://localhost:5000/api/productos'
+//const API = 'http://localhost:5000/api/productos'
+const API = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 export const CartProvider = ({children}) => {
 
